ffc882f7ac69923077010ea8438af672e739eb5e
Ahmed Shaban <ahmed_shaban@mentor.com>
2023-05-26 14:57:04 +0300

    Ch 4: 4 Implement the API - 4.4 Create createPoll() implementing POST.

--- src/main/java/com/example/quickpoll/controller/PollController.java
+++ src/main/java/com/example/quickpoll/controller/PollController.java
@@ -3,10 +3,16 @@ package com.example.quickpoll.controller;
 import com.example.quickpoll.domain.Poll;
 import com.example.quickpoll.repository.PollRepository;
 import jakarta.inject.Inject;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 
 @RestController
@@ -18,5 +24,22 @@ public class PollController {
     public ResponseEntity<Iterable<Poll>> getAllPolls() {
         Iterable<Poll> allPolls = pollRepository.findAll();
         return new ResponseEntity<>(allPolls, HttpStatus.OK);
+    }
+
+    @PostMapping("/polls")
+    public ResponseEntity<Poll> createPoll(@RequestBody Poll poll) {
+
+        poll = pollRepository.save(poll);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(poll.getId())
+                .toUri();
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(uri);
+
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
     }
 }
