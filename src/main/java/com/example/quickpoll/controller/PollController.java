1d79e9f8f1374fc3277145f9fdddc0e5e70b2471
Ahmed Shaban <ahmed_shaban@mentor.com>
2023-05-26 14:33:55 +0300

    Ch 4: 4 Implement the API - 4.3 Create getAll() implementing GET.

--- src/main/java/com/example/quickpoll/controller/PollController.java
+++ src/main/java/com/example/quickpoll/controller/PollController.java
@@ -1,7 +1,11 @@
 package com.example.quickpoll.controller;
 
+import com.example.quickpoll.domain.Poll;
 import com.example.quickpoll.repository.PollRepository;
 import jakarta.inject.Inject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -9,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PollController {
     @Inject
     private PollRepository pollRepository;
+
+    @GetMapping("/polls")
+    public ResponseEntity<Iterable<Poll>> getAllPolls() {
+        Iterable<Poll> allPolls = pollRepository.findAll();
+        return new ResponseEntity<>(allPolls, HttpStatus.OK);
+    }
 }
