cdcdcc8247d98aaaf63872cee26c3fd91ff4ec71
Ahmed Shaban <ahmed_shaban@mentor.com>
2023-06-07 18:40:28 +0300

    Ch 4: Implement the API - 4.12 Create DTOs OptionCount and VoteResult.

---
+++ src/main/java/com/example/quickpoll/controller/dto/OptionCount.java
@@ -0,0 +1,22 @@
+package com.example.quickpoll.controller.dto;
+
+public class OptionCount {
+    private Long optionId;
+    private long count;
+
+    public Long getOptionId() {
+        return optionId;
+    }
+
+    public void setOptionId(Long optionId) {
+        this.optionId = optionId;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+}
