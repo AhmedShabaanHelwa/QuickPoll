44e7d628ef519967675be0838ef277d1daebc452
Ahmed Shaban <ahmed_shaban@mentor.com>
2023-06-07 18:07:31 +0300

    Ch 4: 4 Implement the API - 4.9 Create createPoll implementing POST.

--- src/main/java/com/example/quickpoll/controller/VoteController.java
+++ src/main/java/com/example/quickpoll/controller/VoteController.java
@@ -1,11 +1,38 @@
 package com.example.quickpoll.controller;
 
+import com.example.quickpoll.domain.Vote;
 import com.example.quickpoll.repository.VoteRepository;
 import jakarta.inject.Inject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 public class VoteController {
     @Inject
     VoteRepository voteRepository;
+
+    @PostMapping("/polls/{pollId}/votes")
+    public ResponseEntity<Vote> createVote(@PathVariable Long pollId, @RequestBody Vote vote) {
+
+        Vote createdVote = voteRepository.save(vote);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdVote.getId())
+                .toUri();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(uri);
+
+        return new ResponseEntity<>(vote, httpHeaders, HttpStatus.CREATED);
+    }
 }
